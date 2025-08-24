981cbfecddffe6e88a90ec72f26f1329bc1af8e6
BadetiSriram <sriram.badeti@aaseya.com>
2025-08-24 19:47:34 +0530

    added new file

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly - 5 //ram
+            Save and see your changes instantly - 6 //ram
           </li>
         </ol>
 
